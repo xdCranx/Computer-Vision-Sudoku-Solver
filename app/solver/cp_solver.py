92e92cf30b00429c8e3b6b41a0e5f90b884e7f2e
Łukasz Sokołowski <l.sokolowski2002@gmail.com>
2024-11-12 11:40:14 +0100

    Added CP solver fom Google's OR-TOOLS

---
+++ app/solver/cp_solver.py
@@ -0,0 +1,37 @@
+from ortools.sat.python import cp_model
+
+def cpSolve(puzzle):
+    model = cp_model.CpModel()
+
+    line = list(range(9))
+    box = list(range(3))
+
+    vars_grid = {}
+
+    for i in line:
+        for j in line:
+            vars_grid[i, j] = model.NewIntVar(1, 9, f"cell_{i}_{j}")
+
+
+    for i in line:
+        model.AddAllDifferent([vars_grid[i, j] for j in line])
+        model.AddAllDifferent([vars_grid[j, i] for j in line])
+
+    for i in box:
+        for j in box:
+            model.AddAllDifferent([vars_grid[i * 3 + k, j * 3 + l] for k in box for l in box])
+
+    for i in line:
+        for j in line:
+            if puzzle[i][j] != 0:
+                model.Add(vars_grid[i, j] == puzzle[i][j])
+
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+    print(status)
+    if status == cp_model.OPTIMAL:
+        for i in line:
+            for j in line:
+                puzzle[i][j] = solver.Value(vars_grid[i, j])
+    return puzzle
+
--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,4 +1,4 @@
-from app.solver import solver, lp_solver
+from app.solver import solver, lp_solver, cp_solver
 import app.utils.load_sudoku as load_sudoku
 import pytest
 import numpy as np
@@ -23,3 +23,11 @@ def test_lpSolver(puzzle, solution):
     sudoku = puzzle
     lp_solver.lpSolve(sudoku)
     assert np.array_equal(sudoku, solution)
+
+@pytest.mark.parametrize(
+    "puzzle, solution",
+    [(puzzles[i], solutions[i]) for i in range(len(puzzles))],
+)
+def test_cpSolver(puzzle, solution):
+    cp_solver.cpSolve(puzzle)
+    assert np.array_equal(puzzle, solution)
