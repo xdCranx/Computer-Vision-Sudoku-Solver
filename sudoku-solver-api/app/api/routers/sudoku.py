8c4a2623daf02b2b1b8c1530c93dc3a8dbff210f
Łukasz Sokołowski <l.sokolowski2002@gmail.com>
2024-11-25 22:32:54 +0100

    v0.1

--- sudoku-solver-api/app/api/routers/sudoku.py
+++ sudoku-solver-api/app/api/routers/sudoku.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-
+import numpy as np
+import cv2
 
 router = APIRouter(prefix="/sudoku", tags=["sudoku"])
 
@@ -15,20 +16,14 @@ async def get_placeholder_message():
 
 @router.post("/")
 async def solve_from_array(sudoku: SudokuUploadRequest):
-    from app.solve_from_image import solveFromImage
+    from app.detect_and_solve import detectAndSolve
     import base64
-    decoded_sudoku = base64.b64decode(sudoku.data)
+    decoded_image = base64.b64decode(sudoku.data)
 
-    sudoku = [
-        [5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9],
-    ]
+    np_image = np.frombuffer(decoded_image, dtype=np.uint8)
+    sudoku_image = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
 
-    return sudoku
+
+    solved = detectAndSolve(sudoku_image)
+
+    return solved.tolist()
