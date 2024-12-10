f4cfaf09b3017e1e52694ea404ba3c9db97588d2
Łukasz Sokołowski <l.sokolowski2002@gmail.com>
2024-12-10 01:28:04 +0100

    Added F1-score measurement

--- sudoku-solver-api/app/neural_network/create_model.py
+++ sudoku-solver-api/app/neural_network/create_model.py
@@ -26,6 +26,7 @@ X = np.array(X)
 y = np.array(y)
 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.20, random_state=21
@@ -66,5 +67,14 @@ def digit_recognition_model():
 model = digit_recognition_model()
 model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=20, batch_size=10)
 scores = model.evaluate(X_test, y_test, verbose=0)
+
+y_pred = model.predict(X_test)
+y_pred_classes = np.argmax(y_pred, axis=1)
+y_test_classes = np.argmax(y_test, axis=1)
+
+f1 = f1_score(y_test_classes, y_pred_classes, average="weighted")
+print("F1-score (weighted):", f1)
+
+
 print(scores)
 model.save("./app/neural_network/digit_recognizer_model.keras")
